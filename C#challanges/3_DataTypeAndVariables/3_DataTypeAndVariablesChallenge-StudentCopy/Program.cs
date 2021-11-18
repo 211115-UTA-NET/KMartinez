b0e7394c7c63897405c70f5ba98eb10fb39d4b90
[martinezk23] <[mkaren2301@gail.com]>
2021-11-18 14:37:30 -0600

    add

--- C#challanges/3_DataTypeAndVariables/3_DataTypeAndVariablesChallenge-StudentCopy/Program.cs
+++ C#challanges/3_DataTypeAndVariables/3_DataTypeAndVariablesChallenge-StudentCopy/Program.cs
@@ -16,27 +16,54 @@ namespace _3_DataTypeAndVariablesChallenge
           sbyte mySbyte = -123;
           Console.WriteLine("The value of sbyte:" + mySbyte + "\n");
 
+
           Console.WriteLine("Int Example");
            int myInt = 1;
-           CConsole.WriteLine("The value of Int" = myInt + "\n");
+           Console.WriteLine("The value of Int:" + myInt + "\n");
 
-//           uint myUint = 
 
-//           short myShort = 
+          Console.WriteLine("Uint Example");
+           uint myUint = 12345679;
+           Console.WriteLine("The value of Unint:" + myUint + "\n");
 
-//           ushort myUShort = 
 
-//           float myFloat =
+          Console.WriteLine("Short Example");
+           short myShort = -3284;
+           Console.WriteLine("The value of Short:" + myShort +"\n");
 
-//           double myDouble = 
 
-//           char myCharacter = 
+          Console.WriteLine("ushort Example");
+           ushort myUShort = 0;
+           Console.WriteLine("The value of Ushort:" + myUShort + "\n");
 
-//           bool myBool = 
 
-//           string myText = 
+          Console.WriteLine("Float Example");
+           float myFloat = 16f;
+          Console.WriteLine("The value of Float:" + myFloat +"\n");
 
-//           string numText = 
+
+          Console.WriteLine("Double Example");
+           double myDouble = -1.797639215;
+           Console.WriteLine("The value of Double:" + myDouble + "\n");
+
+
+          Console.WriteLine("Char Example");
+           char myCharacter = '!';
+           Console.WriteLine("Character:" + myCharacter + "\n");
+
+          Console.WriteLine("Bool Example");
+           bool myBool = true;
+           Console.WriteLine(myBool ? "Checked:" : "Not Checked:" + myBool);
+           Console.WriteLine(false ? "Checked:" : "Not Checked:" + myBool + "\n");
+
+
+          Console.WriteLine("1st String Example");
+           string myText1 = "This is the 1st String, I hope.";
+           Console.WriteLine("" + myText1 + "\n");
+
+          Console.WriteLine("2nd String  Example");
+           string numText2 = "This should be the 2nd String 0010, hopefully.";
+           Console.WriteLine("" + numText2);
       }
 
       public static int Text2Num(string numText)
