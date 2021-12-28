f4004b1c0c7f872d9d1b475e8565ae20da26c11e
[martinezk23] <[mkaren2301@gail.com]>
2021-12-28 00:16:18 -0600

    attempt option 2 asp.net core

--- WK5.5/.aspnetcoreExcersies/AspNetCoreExcersies/Program.cs
+++ WK5.5/.aspnetcoreExcersies/AspNetCoreExcersies/Program.cs
@@ -1,7 +1,41 @@
+//Exce// exercise option 1:
+//    make an asp.net core app (start with the empty template) that
+//      keeps track of some structured data in custom classes and uses serialization
+//      to respond with the data. for example: static list of rock-paper-scissors roundresults,
+//    and use XmlSerializer to convert it to XML in the response body.
+// exercise option 2:
+//   make an asp.net core app (start with the empty template) that
+//     treat the "path" as a relative path on the server's file system.
+//     the response should include the contents of that file.
+//     example: https://localhost:7206/Program.cs
+//     choose a good content type based on the file extension.
+//     return 404 if no such file.
+// 
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!" + "goodbye");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<KestrelServerOptions>(options => options.AllowSynchronousIO = true);
+WebApplication app = builder.Build();
 
-app.Run();
+app.MapGet("/", () => "Hello World!" + "goodbye"); //to check if program works
+
+app.Run(context =>
+{
+    //Exercise option 2
+    string path = context.Request.Path;
+    string fileName = $"Exercise2.txt";
+    string filePath = $"C:/Users/mkare_wyte20y/Revature/KarenM/WK5.5/.aspnetcoreExcersies/AspNetCoreExcersies/Excercise2.txt";
+
+    context.Response.StatusCode = 404;
+    context.Response.Body.Write(Encoding.UTF8.GetBytes($"path was: {path}, data was {filePath}, filename: {fileName}"));
+    return Task.CompletedTask;
+});
+
+object FileInfo(string filePath, string fileName)
+{
+    throw new NotImplementedException();
+}
+
+app.Run();//Runs app
