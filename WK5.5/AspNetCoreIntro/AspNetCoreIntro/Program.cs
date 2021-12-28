b4406b7733ea2ec169d8d08645e0af4a0e449156
[martinezk23] <[mkaren2301@gail.com]>
2021-12-27 22:47:22 -0600

    adding changes

--- WK5.5/AspNetCoreIntro/AspNetCoreIntro/Program.cs
+++ WK5.5/AspNetCoreIntro/AspNetCoreIntro/Program.cs
@@ -1,6 +1,60 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Text;
+//This is me trying to follow along so far most of the code worked.
 
-app.MapGet("/", () => "Hello World!");
 
+//webapplication builder
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<KestrelServerOptions>(options => options.AllowSynchronousIO = true);
+WebApplication app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");  //opens on browser
+
+
+/*After you have a web application you need to construct its
+request-processing pipepline using components called "middlewares"
+*/
+
+
+//app.Use((context, next) =>
+//{
+//    if (context.Request.Query["authenticated"] == "true")
+//    {
+//        //This middleware is done, let the next one in the piepline take over
+//        next(context);
+//    }
+//    else
+//    {
+//        context.Response.StatusCode = 401;
+//        context.Response.ContentType = "text/plain";
+//        context.Response.Body.Write(Encoding.UTF8.GetBytes("errors, not authenticated"));
+//    }
+
+    
+//    return Task.CompletedTask;
+//});
+
+/*this call add a middlewar to pipline.
+each middleware runs in sequences*/
+app.Run(context =>
+{
+
+
+
+
+
+    string path = context.Request.Path;
+    //string queryValue = context.Request.Query["data"];
+    string dataValue = context.Request.Query["data"];
+
+    //the HttpContext parameter 
+    context.Response.StatusCode = 200;
+   // context.Response.Body.Write($"path was: {path}, data was {}");
+    context.Response.Body.Write(Encoding.UTF8.GetBytes($"path was: {path}, data was {dataValue}"));
+    //need this line so it can comple(really thsi delgate should be aysnc but we haven't done that yet)
+    return Task.CompletedTask;
+});
+
+//similar to Console.ReadLine()
+//this one runs the app
 app.Run();
